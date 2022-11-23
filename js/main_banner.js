3113d6e069783e07e500a0b6723231f0b7baa80a
User -2022RYNXA\Administrator <hsykrw@naver.com>
2022-11-23 13:11:12 +0900

    no message

--- js/main_banner.js
+++ js/main_banner.js
@@ -4,22 +4,26 @@ let slides = document.querySelector('.main-banner'),
     slide = document.querySelectorAll('.main-banner li'),
     currentIdx = 0,
     slideCount = slide.length,
-    container = document.querySelector('.slide-container')
-    prevBtn = container.querySelector('.btn_left'),
-    nextBtn = container.querySelector('.btn_right');
+    container = document.querySelector('.slide-container'),
+    prevBtn = container.querySelector('.btn_left img'),
+    nextBtn = container.querySelector('.btn_right img');
 
 function moveSlide(num) {
-    slides.style.left = -num * 100 +'%';
+    slides.style.left = -num * 100 + '%';
     currentIdx = num;
-} 
+}
 
-container.addEventListener('click', function (e) {
+nextBtn.addEventListener('click', function (e) {
     // e.preventDefault();
     console.log(e.target);
     if (currentIdx < slideCount - 1) {
         moveSlide(currentIdx + 1);
-        console.log(currentIdx);
     }
 });
 
-console.log(document.querySelector('.btn_left'));
+prevBtn.addEventListener('click', function(e) {
+    console.log(e.target);
+    if (currentIdx > slideCount - 1) {
+        moveSlide(currentIdx - 1);
+    }
+});
