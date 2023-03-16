e5617627660085610acf21a46bb3e57342d27a25
DESKTOP-UITD4BN\gumi3 <gumi368@naver.com>
2023-03-16 09:24:42 +0900

    d

--- mealbong/src/main/java/com/project/mealbong/product/ProductController.java
+++ mealbong/src/main/java/com/project/mealbong/product/ProductController.java
@@ -37,9 +37,6 @@ public class ProductController {
         cri.setCurrPage(currPage);
         cri.setOrderKey(orderKey);
 
-        System.out.println("엄상호 화이팅!!!" + cri);
-        System.out.println("송강현 와이팅!!" +  productService.criList(cri));
-
         mv.addObject("productList", productService.criList(cri));
         mv.addObject("code_number", productService.categoryList());
 
